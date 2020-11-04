59a83ebefd3d741e0910f73c12306a12dd508078
Ioxom <jackwheppner@gmail.com>
2020-11-03 19:35:16 -0600

    begin part 5

---
+++ stuff/src/ca/ioxom/part5/learningObjectOrientedProgramming/Book.java
@@ -0,0 +1,34 @@
+package ca.ioxom.part5.learningObjectOrientedProgramming;
+
+public class Book {
+    //constructor
+    private final String author;
+    //renamed "name" to "title". thought it made more sense
+    private final String title;
+    private final int pages;
+    public Book(String author, String name, int pages) {
+        this.author = author;
+        this.title = name;
+        this.pages = pages;
+    }
+
+    //get info
+    public String getAuthor() {
+        return this.author;
+    }
+    public String getTitle() {
+        return this.title;
+    }
+    public int getPages() {
+        return this.pages;
+    }
+
+    public String toString() {
+        return this.author + ", " + this.title + ", " + this.pages + " pages";
+    }
+
+    public static void main(String[] arrgs) {
+        Book john = new Book("jhon", "john", 6483798);
+        System.out.println(john);
+    }
+}
---
+++ stuff/src/ca/ioxom/part5/learningObjectOrientedProgramming/Fitbyte.java
@@ -0,0 +1,12 @@
+package ca.ioxom.part5.learningObjectOrientedProgramming;
+
+public class Fitbyte {
+    private final int age;
+    private final int restingHeartRate;
+    public Fitbyte(int age, int restingHeartRate) {
+        this.age = age;
+        this.restingHeartRate = restingHeartRate;
+    }
+
+
+}
