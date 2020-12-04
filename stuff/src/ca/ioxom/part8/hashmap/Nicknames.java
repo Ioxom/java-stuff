ebe6aa0a5c34416fa2313b7c67644449ea1037bc
Ioxom <jackwheppner@gmail.com>
2020-12-04 16:04:51 -0600

    fix Nicknames.java idea was borked and didn't save some things

--- stuff/src/ca/ioxom/part8/hashmap/Nicknames.java
+++ stuff/src/ca/ioxom/part8/hashmap/Nicknames.java
@@ -6,10 +6,9 @@ public class Nicknames {
     public static void main(String[] arrgs) {
         HashMap<String, String> nicknames = new HashMap<>();
         String[] names = {"matthew", "matt", "michael", "mix", "arthur", "artie"};
-        
-        nicknames.put("matthew", "matt");
-        nicknames.put("michael", "mix");
-        nicknames.put("arthur", "artie");
+        for (int i = 0; i < 6; i += 2) {
+            nicknames.put(names[i], names[i + 1]);
+        }
         //just added a print to remove the warning
         System.out.println(nicknames);
     }
